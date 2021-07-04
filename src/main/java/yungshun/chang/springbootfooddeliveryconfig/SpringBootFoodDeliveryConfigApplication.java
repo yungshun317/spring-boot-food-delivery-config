f549dc7fdb94701a31631e924fd4b81e0fa446a2
yungshun317 <seraph317@gmail.com>
2021-07-04 10:55:56 +0800

    [CHORE] Initialize a Spring Boot project

---
+++ src/main/java/yungshun/chang/springbootfooddeliveryconfig/SpringBootFoodDeliveryConfigApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springbootfooddeliveryconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFoodDeliveryConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFoodDeliveryConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yungshun/chang/springbootfooddeliveryconfig/SpringBootFoodDeliveryConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springbootfooddeliveryconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFoodDeliveryConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
