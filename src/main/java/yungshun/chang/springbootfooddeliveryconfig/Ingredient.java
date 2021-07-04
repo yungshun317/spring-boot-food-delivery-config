3c6e2542353a9410f4e889b1ae39abcf2618ebbf
yungshun317 <seraph317@gmail.com>
2021-07-04 10:58:46 +0800

    [FEAT] Define the `Ingredient` model class

---
+++ src/main/java/yungshun/chang/springbootfooddeliveryconfig/Ingredient.java
@@ -0,0 +1,12 @@
+package yungshun.chang.springbootfooddeliveryconfig;
+
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
