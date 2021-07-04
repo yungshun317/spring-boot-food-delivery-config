57825be628adb9169a5f975561dd717dff75f0d2
yungshun317 <seraph317@gmail.com>
2021-07-04 11:01:28 +0800

    [FEAT] Generate boilerplate code with `Lombok`

--- src/main/java/yungshun/chang/springbootfooddeliveryconfig/Ingredient.java
+++ src/main/java/yungshun/chang/springbootfooddeliveryconfig/Ingredient.java
@@ -1,7 +1,18 @@
 package yungshun.chang.springbootfooddeliveryconfig;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Id;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PUBLIC, force=true)
 public class Ingredient {
 
+    @Id
     private final String id;
     private final String name;
     private final Type type;
