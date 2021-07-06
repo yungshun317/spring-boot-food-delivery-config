6f53992bb65d01494dce460581f033da2fb98d30
yungshun317 <seraph317@gmail.com>
2021-07-06 13:35:39 +0800

    [FEAT] Use the `@Digits`, `@NotBlank`, and `@Pattern` annotations for validation

--- src/main/java/yungshun/chang/springbootfooddeliveryconfig/Order.java
+++ src/main/java/yungshun/chang/springbootfooddeliveryconfig/Order.java
@@ -1,8 +1,9 @@
 package yungshun.chang.springbootfooddeliveryconfig;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,24 +19,33 @@ public class Order implements Serializable {
 
     private Date placedAt;
 
+    @NotBlank(message="Delivery name is required")
     private String deliveryName;
 
+    @NotBlank(message="Street is required")
     private String deliveryStreet;
 
+    @NotBlank(message="City is required")
     private String deliveryCity;
 
+    @NotBlank(message="State is required")
     private String deliveryState;
 
+    @NotBlank(message="Zip code is required")
     private String deliveryZip;
 
     private String ccNumber;
 
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message="Must be formatted MM/YY")
     private String ccExpiration;
-    
+
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
+    @ManyToMany(targetEntity=Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
+    @PrePersist
     public void addDesign(Taco design) {
         this.tacos.add(design);
     }
